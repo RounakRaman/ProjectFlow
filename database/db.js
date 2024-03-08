4d49652d996a520d7350f2bc11259f5a74070d21
Rounak Raman <raman.rounak@gmail.com>
2024-03-09 00:55:03 +0530

    Connected to MongoDb Atlas

--- database/db.js
+++ database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://127.0.0.1:27017/task-manager';
+const mongoURI = 'mongodb+srv://mern-vercel:<password>@mern-vercel.tqsdz6e.mongodb.net/task-manager';
 const options = {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
